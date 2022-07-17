abe84f9b3b83018679ff166acca38509fecf7ef0
Александр Соловьев <aleksandrsolovev@Aleksandrs-MacBook-Air.local>
2022-07-17 20:41:17 +0300

    zada4i

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// double x = 2.09;
+// Console.WriteLine(x);
+// Console.ReadLine();
+
+//2.
+
+
+// string a = Console.ReadLine();
+// Console.Write($"Введенное вами число {(a)}");
+
+//3
+// string a = Console.ReadLine();
+// Console.Write($" {(a)} Вот какое чмсло вы ввели");
+//1.5
+
+
+// Console.WriteLine("1 13 49");
+//Console.WriteLine("1  13  49");
+
+// Random rnd = new Random();
+// int value = rnd.Next(1 , 10);
+// int value1 = rnd.Next(10 , 20);
+// int value2 = rnd.Next(30 , 40);
+// Console.WriteLine($"{value}  {value1}  {value2}");
+
+// Console.WriteLine("10");
+// Console.WriteLine("50");
+
+// Console.WriteLine("2кг");
+// Console.WriteLine("13 17");
+
+
+// S2.1. Составить программу:
+// а) вычисления значения функции y=7x2+3x+6 при любом значении x;
+
+// S2.1.
+//а) вычисления значения функции y = 7x ^ 2 + 3x + 6 при любом значении x;
+//б) вычисления значения функции x = 12a ^ 2 + 7a + 12 при любом значении а.
+
+// Random random = new Random();
+
+// double y, x1;
+// double x = random.Next(-100, 100);
+// double a = random.Next(-100, 100);
+// y = ((7 * Math.Pow(x, 2)) + (3 * x) + 6);
+// x1 = ((12 * Math.Pow(a, 2)) + (7 * a) + 12);
+// Console.WriteLine($"((7 * {Math.Pow(x, 2)}) + (3 * {x}) + 6)= {y}");
+// Console.WriteLine($"((12 * {Math.Pow(a, 2)}) + (7 * {a}) + 12)= {x1}");
+// Console.ReadKey();
